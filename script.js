91bf50c50dbb68ffa883453ff7914a202df123e1
zwozaiden-glitch <zwozaiden@gmail.com>
2026-08-22 21:32:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
    ========================================================================== */
 const DISCORD_INVITE = "https://discord.gg/xFeX95Evce";
 const API_HOST = "https://discord-project-production-cc27.up.railway.app";
-const API_TOKEN = "YOUR_API_TOKEN";
+const API_TOKEN = "A2F7o-nCC04ed2SrUMftZmXQJ37qvvEn";
 const SCRIPT_NAME = "Vmax";
 const LOADER_PATH = "/api/v1/load";
 const DASHBOARD_URL = "https://discord-project-production-cc27.up.railway.app/dashboard";
